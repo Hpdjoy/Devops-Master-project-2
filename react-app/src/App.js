e936baf1aed9ae2cdd3094bbba3376373010db01
hpdjoy <hp0event@gmail.com>
2025-07-27 15:58:21 +0530

    � Fixed: React app structured, Docker + CI ready

---
+++ react-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>🚀 React DevSecOps App</h1>
+      <p>Hello Harsha bhai! This is your CI/CD demo 😎</p>
+    </div>
+  );
+}   
+
+export default App;
